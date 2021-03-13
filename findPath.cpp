eb8ffffb91a2b43b065438eb3de08d3f77e9b557
Aditya Jain <adi.jain059@gmail.com>
2021-03-14 02:12:34 +0530

    some bugs fixed in findPath

--- findPath.cpp
+++ findPath.cpp
@@ -18,9 +18,16 @@ void find_path(string inputToLevel, string ans, int level, float totalBias) {
 	string inputToS_box;
 	string outputOfLevel;
 	string outputOfS_box;
+	string result;
 	bool flag1, flag2;
 	
+	for(int i = 0; i < plainTextSize; i++) {
+		if(inputToS_box[i] == '1') {
+			result += "K" + to_string(level) + to_string(i + 1) + " ";
+	}
+	
 	for(int k = 1; k < totalInputCombination; k++) {
+		
 		outputOfLevel = bitset<plainTextSize>(k).to_string(); //to binary
 			
 		for(int i = 0; i < plainTextSize; i += SboxSize) {
@@ -32,7 +39,6 @@ void find_path(string inputToLevel, string ans, int level, float totalBias) {
 			flag2 = false;
 			for(int j = 0; j < SboxSize; j++) {
 				if(inputToS_box[j] == '1') {
-					ans += "K" + to_string(level) + to_string(i + j + 1) + ",";
 					for(int l = 0; l < 8; l++) {
 						xorValue[l] = xorValue[l] ^ S_box[j][l];
 					}
@@ -52,29 +58,40 @@ void find_path(string inputToLevel, string ans, int level, float totalBias) {
 				}
 			}
 			
-			if(flag1 == false && flag2 == true)
+			if((flag1 == false && flag2 == true) || (flag1 == true && flag2 == false))
 				break;
 			
-			int sum = 0;
-			for(int j = 0; j < 8; j++) {
-				if(xorValue[j] == '0')
-					sum++ ;
+			if(flag1 == true && flag2 == true) {
+				int sum = 0;
+				for(int j = 0; j < 8; j++) {
+					if(xorValue[j] == '0')
+						sum++ ;
+				}
+				
+				float bias = sum / 8.0 - 0.5;
+				
+				totalBias *= (bias * 2);
+				
+				if(totalBias == 0) {
+					// More Optimisation can be done here
+					// k += (2 << (plainTextSize - i - SboxSize)) - 1;
+					break;
+				}
 			}
-			
-			float bias = sum / 8.0 - 0.5;
-			
-			totalBias *= (bias * 2);
 		}
-		if(flag1 == false && flag2 == true)
+		if((flag1 == false && flag2 == true) || (flag1 == true && flag2 == false) || totalBias == 0) {
+			// More Optimisation can be done here
+			// k += (2 << (plainTextSize - i - SboxSize)) - 1;
 			continue;
+		}
 			
 		for(int i = 0; i < plainTextSize; i++) {
 			inputToLevel[i] = outputOfLevel[Permutation[i]];
 		}
 		
 		// recursive case 
-		find_path(inputToLevel, ans, level + 1, totalLevels);
-
+		find_path(inputToLevel, ans + result, level + 1, totalBias);
+	
 	}
 		
 }
@@ -88,21 +105,21 @@ int main() {
 	S_box[3] = "00011110";
 	S_box[4] = "01001011";
 	S_box[5] = "00101101";
-	float totalBias = 0.5; 
 	
 	for(int i = 1; i < totalInputCombination; i++)
 	{
+		float totalBias = 1; 
 		string inputToLevel = bitset<plainTextSize>(i).to_string(); //to binary
 		cout << inputToLevel << endl;
 		string ans = "";
 		for(int j = 0; j < plainTextSize; j++) {
 			if(inputToLevel[j] == '1') {
-				ans += "P" + to_string(j + 1) + ",";
+				ans += "P" + to_string(j + 1) + " ";
 			}
 		}
 		cout << ans << endl;
 		find_path(inputToLevel, ans, 0, totalBias);
-		cout << ans << endl;
+		//cout << ans << endl;
 	}
 	
 }
